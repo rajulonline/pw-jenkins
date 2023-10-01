9aebd2679a780cafffb31cb25560808f38102a39
LAKSHMI NARASIMHAN RAGHAVAN <lakshminarasimhanraghavan@LAKSHMIs-MacBook-Pro.local>
2023-09-30 21:37:39 -0700

    simple test edit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,6 +5,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveURL(/playwright/);
 });
 
 test('get started link', async ({ page }) => {
