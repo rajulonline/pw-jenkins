43a5aac1d00666f3be1b56d5be4f0667fd0a9e7b
LAKSHMI NARASIMHAN RAGHAVAN <lakshminarasimhanraghavan@LAKSHMIs-MacBook-Pro.local>
2023-10-02 10:08:41 -0700

    simple test edit to validate the token

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+const gittoken = process.env.BEARER_TOKEN;
+console.log(`This is the gittoke ${gittoken}`);
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Tamil/);
   await expect(page).toHaveURL(/playwright/);
